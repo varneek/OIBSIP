9459c8000e552c8cf1124f4af0a88f61bce44ae9
varneek <154414997+varneek@users.noreply.github.com>
2023-12-27 16:16:19 +0530

    weather_api.py

--- weather_api.py
+++ weather_api.py
@@ -1,20 +1,59 @@
+import tkinter as tk 
 import requests
-import json
+from tkinter import messagebox
+
 
 api_key = '4a29ff08445ca33653d638a16b3e7911'
-city = input('Enter the city:- ')
-
-data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}')
 
 
-if data.json()['cod'] == '404':
-    print('City not found')
-else:
-    weather = data.json()['weather'][0]['main']
-    temp = data.json()['main']['temp']
-    humidity = data.json()['main']['humidity']
+def get_weather(city):
+    data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}')
+    if data:
+        city = data.json()['name']
+        country = data.json()['sys']['country']
+        weather = data.json()['weather'][0]['main']
+        temp = data.json()['main']['temp']
+        temp = round(temp - 273.15, 2)
+        icon = data.json()['weather'][0]['icon']
+        result = (city, country, weather, temp, icon)
+        return result
 
 
-    print(f'Weather of {city}: {weather}')
-    print(f'Temperature of {city}: {temp}°F')
-    print(f'Humidity of {city}: {humidity}')
+def search():
+    city = main_city.get()
+    weather = get_weather(city)
+    if weather:
+        location['text'] = f'{weather[0]}, {weather[1]}'
+        main_weather['text'] = f'{weather[2]}'
+        main_temp['text'] = f'{weather[3]}°C'
+        image['image'] = f'E:/code/weather_app/icons/{weather[4]}.png'
+        
+    else:
+        messagebox.showerror('Error', f'{city} city not found')
+        
+
+app = tk.Tk()
+app.title('Weather app')
+app.geometry('600x350')
+
+main_city = tk.StringVar()
+city_entry = tk.Entry(app, textvariable=main_city, width=30)
+city_entry.pack()
+
+search_bt = tk.Button(app, text='search weather', width= 15, command= search)
+search_bt.pack()
+
+location = tk.Label(app, text='', font=('bold', 20))
+location.pack()
+
+image = tk.Label(app, image='')
+image.pack(padx=20,pady=30)
+
+main_temp = tk.Label(app, text='', font=('bold'))
+main_temp.pack()
+
+main_weather = tk.Label(app, text='', font=('bold'))
+main_weather.pack()
+
+
+app.mainloop()
